413ee3649c07ca60685132a7ba6cadc64c30b96e
Israel-Millán7 <israelmilllan7@gmail.com>
2021-01-05 11:44:39 -0300

    ejercicio basico java

---
+++ Java/ProyectosJava/src/com/talento/java/proyecto5/TestBasic.java
@@ -0,0 +1,21 @@
+package com.talento.java.proyecto5;
+
+public class TestBasic {
+
+	public static void main(String[] args) {
+		BasicJava testPrint = new BasicJava();
+		testPrint.printNumber();
+		testPrint.printImpar();
+		testPrint.printSum();
+		testPrint.arreglo();
+		testPrint.arrMax();
+		testPrint.promPrint();
+		testPrint.arrayImpares();
+		testPrint.mayorY();
+		testPrint.cuadrado();
+		testPrint.numNeg();
+		testPrint.nuevoArray();
+		testPrint.IntercambiaValores();
+	}
+
+}
